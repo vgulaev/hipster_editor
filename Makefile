1ced879279ad5c03d318f0f8299f3c103ebd8212
vgulaev <vgulaev@yandex.ru>
2023-05-14 22:31:58 +0500

    Up

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
-srcdir	 =src/
+srcdir	 = src/
 TARGETS	 = hipster
+INC=-I/mingw64/include/
 
 .PHONY: all
 all: $(TARGETS)
 
-hipster: $(srcdir)debug.c $(srcdir)window.c $(srcdir)main.c
-	$(CC) $(srcdir)debug.c $(srcdir)window.c $(srcdir)main.c -lSDL2 -o $@
+FILE_LIST = $(srcdir)window.c $(srcdir)main.c
+
+ifeq ($(OS),Windows_NT)
+	PARAMS_LIST = $(INC) $(FILE_LIST) -mwindows -lmingw32 -lSDL2main -L/mingw64/lib
+else
+  PARAMS_LIST = -lSDL2 $(FILE_LIST)
+endif
+
+hipster: $(FILE_LIST)
+	$(CC) $(PARAMS_LIST) -lSDL2 -o $@
 
 .PHONY: clean
 clean:
--- src/main.c
+++ src/main.c
@@ -1,24 +1,25 @@
 // #include <stdio.h>
 #include <SDL2/SDL.h>
 
-#include "debug.h"
+// #include "debug.h"
 #include "window.h"
 
-int main()
-{
+void main_main() {
   window_init();
 
   draw_rect(50, 50, 50, 50);
   draw_rect(100, 100, 50, 50);
 
   quit();
+}
 
-  // cdg();
-  // printf("Cores %ld\n", number_of_processors);
-
-  // //#pragma omp parallel
-  // #pragma omp single
-  // printf("Hello, 66 world.\n");
-  // printf("Hello, World!+++");
+int main(int, char **) {
+  main_main();
   return 0;
 }
+
+int WinMain(int, char **) {
+  main_main();
+  return 0;
+}
+// --------
